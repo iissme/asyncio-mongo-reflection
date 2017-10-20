91312a4c81ffdb109080eb2f1207659d72900c6e
Alexander Shlyakhov <ishlyakhov@gmail.com>
2017-10-20 17:46:22 +0300

    Fix redundant logging config.

--- asyncio_mongo_reflection/__init__.py
+++ asyncio_mongo_reflection/__init__.py
@@ -15,11 +15,4 @@ __license__ = 'MIT'
 __copyright__ = 'Copyright 2017 isanich'
 __version__ = '0.0.1'
 
-try:
-    from logging import NullHandler
-except ImportError:
-    class NullHandler(logging.Handler):
-        def emit(self, record):
-            pass
-
-logging.getLogger(__name__).addHandler(NullHandler())
+logging.getLogger(__name__).addHandler(logging.NullHandler())
